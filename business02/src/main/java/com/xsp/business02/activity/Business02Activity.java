112cdefe90f0d70c02d749e220aee18b9b6b3646
xushaopeng <xushaopeng@baidu.com>
2016-11-17 22:35:20 +0800

    add aclick event

--- business02/src/main/java/com/xsp/business02/activity/Business02Activity.java
+++ business02/src/main/java/com/xsp/business02/activity/Business02Activity.java
@@ -13,18 +13,19 @@ public class Business02Activity extends BaseActivity implements View.OnClickList
         super.onCreate(savedInstanceState);
         setContentView(R.layout.bussiness02_activity);
 
-        findViewById(R.id.id_jump_to_main).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                UrlRouter.from(Business02Activity.this).jump("activity://activity/main");
-            }
-        });
+        findViewById(R.id.id_jump_to_main).setOnClickListener(this);
         findViewById(R.id.id_jump_to_self).setOnClickListener(this);
         findViewById(R.id.id_jump_to_business_one).setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
-
+        if (view.getId() == R.id.id_jump_to_main) {
+            UrlRouter.from(Business02Activity.this).jump("activity://activity/main");
+        } else if (view.getId() == R.id.id_jump_to_main) {
+            UrlRouter.from(Business02Activity.this).jump("activity://native/main");
+        } else if (view.getId() == R.id.id_jump_to_business_one) {
+            UrlRouter.from(Business02Activity.this).jump("activity://main");
+        }
     }
 }
